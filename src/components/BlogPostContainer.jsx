d237d18842c8a05f9b6e9cbc2ea3781d209bd113
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-12 09:08:24 +0000

    update

--- src/components/BlogPostContainer.jsx
+++ src/components/BlogPostContainer.jsx
@@ -5,7 +5,7 @@ export default function BlogPostContainer(props) {
             <h4 className="card-title">{props.blogPost.title}</h4>
             <p>{new Date(props.blogPost.createdAt).toLocaleDateString()}</p>
             <p className="card-text">{props.blogPost.markdown.slice(0,200)}...</p>
-            <a href="https://mjclifford-blog.herokuapp.com/" className="btn btn-sm btn-st">Read more</a>
+            <a href="https://mjclifford-blog.herokuapp.com/" className="btn btn-sm btn-st" target="_blank" rel="noreferrer">Read more</a>
 
         </div>
     )
