454ea0de780774be103db259afbffe425daf6d2b
Matthew Clifford <matthewclifford@hotmail.co.uk>
2023-01-18 08:25:27 +0000

    update to page not found

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,11 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+            <div className="container mb-4">
+              <h1>Whoops!</h1>
+              <p>It looks like that page doesn't exist.</p>
+              <p>Please return to the home page and try again.</p>
+            </div>
+    );
   };
   
   export default NoPage;
