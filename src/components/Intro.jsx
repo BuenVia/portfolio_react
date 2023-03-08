9814f8871f2b4b8e389ada2fc3b2173f4ce8818e
SaracensFan <matthewclifford@hotmail.co.uk>
2023-03-08 19:36:04 +0000

    update to languages

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
                     </div>
                     <div className="col-md-6 p-2">
                         <p>Hi, I am Matt 👋. A self-taught WebDev with a passion for code 💻.</p> 
-                        <p>I have been self-teaching myself JavaScript, HTML, CSS, NodeJS and React with a strong desire to move into a career in coding 👨‍💻.</p>
+                        <p>I have been self-teaching myself JavaScript, HTML, CSS, NodeJS, React and Python with a strong desire to move into a career in coding 👨‍💻.</p>
                         <p>If you want me to build your website or webapp, or you want to collaborate, then get in touch! 🗣️</p>
                         <p>📧 Email: <a href="mailto:matthewclifford@hotmail.co.uk" className="email">matthewclifford@hotmail.co.uk</a></p>
                     </div>
