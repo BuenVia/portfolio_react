94dba4491c1504513ba98661dba98ab3f312081c
Matt Clifford <matthewclifford@hotmail.co.uk>
2023-09-20 21:45:56 +0100

    removal of redundant features

--- src/components/Intro.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Intro from "./Intro"
-import React from "react"
-
-test("input field ok", () => {
-    render(<Intro />)
-    const header = screen.getByText('About')
-})
