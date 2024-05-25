4d232e478718f4fec0670cf7e27e8b450887663c
Matt Clifford <matthewclifford@hotmail.co.uk>
2024-05-25 15:08:49 +0100

    update

--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+// import { useRef } from "react"
 import emailjs from '@emailjs/browser'
 
 const Wedding = () => {
@@ -11,6 +12,7 @@ const Wedding = () => {
         dietry: "",
         address: ""
     })
+    // const form = useRef()
 
     const updateForm = (e) => {
         const { name, value } = e.target
@@ -23,11 +25,23 @@ const Wedding = () => {
     }
 
     const handleSend = (e) => {
-        e.preventDefault()
-        // if (email.attend === "" | email.firstname === "" | email.lastname === "" | email.guests === "" | email.dietry === "" | email.address === "") {
-
-        // }    
-        emailjs.send('service_if35bh3', 'template_si55oe6', email, { publicKey: 'hvIxhJA1XhTZHHkL5' }).then(
+        e.preventDefault()    
+        // console.log(form.current);
+        emailjs
+        .send(
+            'service_if35bh3', 
+            'template_si55oe6', 
+            {
+                attend: email.attend,
+                firstname: email.firstname,
+                lastname: email.lastname,
+                guests: email.guests,
+                dietry: email.dietry,
+                address: email.address
+            },
+            { publicKey: 'hvIxhJA1XhTZHHkL5' }
+        )
+        .then(
             alert("Thanks for sending... you can now navigate away.")
         )
     }
@@ -39,10 +53,11 @@ const Wedding = () => {
             <p>Saturday 28th September 2024</p>
             <p>Valencia, Spain</p>
 
-            <form onSubmit={handleSend} className="wedding__form">
+            {/* <form ref={form} onSubmit={handleSend} className="wedding__form"> */}
+            <form action="" className="wedding__form" onSubmit={handleSend}>
                 <div className="wedding__div">
-                    <label htmlFor="">Attending?</label>
-                    <select onChange={updateForm} name="" id="attend" required>
+                    <label htmlFor="" onChange={updateForm}>Attending?</label>
+                    <select onChange={updateForm} name="attend" id="attend" required>
                         <option value="">---PLEASE SELECT---</option>
                         <option value="yes">Yes</option>
                         <option value="no">No</option>
@@ -50,25 +65,25 @@ const Wedding = () => {
                 </div>
                 <div className="wedding__div">
                     <label htmlFor="">First Name</label>
-                    <input onChange={updateForm} type="text" name="firstname" id="" value={email.firstname} required/>
+                    <input onChange={updateForm} type="text" name="firstname" id="" required/>
                 </div>
                 <div className="wedding__div">
                     <label htmlFor="">Last Name</label>
-                    <input onChange={updateForm} type="text" name="lastname" id="" value={email.lastname} required/>
+                    <input onChange={updateForm} type="text" name="lastname" id="" required/>
                 </div>
                 <div className="wedding__div">
-                    <label htmlFor="">Number of Guests</label>
-                    <input onChange={updateForm} type="number" name="guests" id="" value={email.guests} required/>
+                    <label htmlFor="">Number of Guests (if just you, select 1 - if not coming write 0)</label>
+                    <input onChange={updateForm} type="number" name="guests" id="" required/>
                 </div>
                 <div className="wedding__div">
                     <label htmlFor="">Dietry Requirements</label>
-                    <textarea onChange={updateForm} name="dietry" id="" value={email.dietry}></textarea>
+                    <textarea onChange={updateForm} name="dietry" id="" ></textarea>
                 </div>
                 <div className="wedding__div">
-                    <label htmlFor="">Address (to send the invite)</label>
-                    <textarea onChange={updateForm} name="address" id="" value={email.address} required></textarea>
+                    <label htmlFor="">Address (to send the invite - if not coming write 'N/A')</label>
+                    <textarea onChange={updateForm} name="address" id="" required></textarea>
                 </div>
-                <button type="submit" className="btn btn-sm">Submit</button>
+                <input type="submit" value="Send" />
             </form>
         </div>
     )
