7e4797cf34ebe77aca9e5955c0887944dece3387
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-02-24 18:04:00 +0000

    minor change to text and card outlines

--- src/languages.js
+++ src/languages.js
@@ -1,11 +1,11 @@
 const language = {
     engIntro: [
-        "Hi, I am Matt 👋. A professional Software Programmer with a passion for code 💻.",
+        "Hi, I am Matt 👋. A professional Software Engineer with a passion for code 💻.",
         "I currently work with Python, JavaScript, NodeJS, React, PHP, HTML, CSS, have a strong desire to progress my career in software development 👨‍💻.",
         "If you want me to build your website, API or any other webapp, or you want to collaborate, then get in touch! 🗣️",
     ],
     espIntro: [
-        "Hola, me llamo Matt 👋. Yo soy un desarollador de sfotware professional con un montón de pasión por escribir código! 💻",
+        "Hola, me llamo Matt 👋. Yo soy un ingeniero de sfotware professional con un montón de pasión por escribir código! 💻",
         "Me he enseñado JavaScript, HTML, CSS, NodeJS, React, PHP y Python y tengo muchísimas ganas de progresar como un programador profesional. 👨‍💻",
         "Si quieres que te produzca una página web o ‘webapp’ o quizá quieres charlar sobre una idea que tengas, ponte en contacto conmigo. 🗣️"
     ]
